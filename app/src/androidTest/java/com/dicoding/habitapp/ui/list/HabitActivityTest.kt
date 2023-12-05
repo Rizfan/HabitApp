b86de7956c8717f0788a2ad8230806ed399574f5
Rizfan <rispanherlaya@gmail.com>
2023-12-05 14:44:10 +0700

    Initial commit

---
+++ app/src/androidTest/java/com/dicoding/habitapp/ui/list/HabitActivityTest.kt
@@ -0,0 +1,37 @@
+package com.dicoding.habitapp.ui.list
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import com.dicoding.habitapp.R
+import com.dicoding.habitapp.ui.add.AddHabitActivity
+
+//TODO 16 : Write UI test to validate when user tap Add Habit (+), the AddHabitActivity displayed
+@RunWith(AndroidJUnit4ClassRunner::class)
+class HabitActivityTest {
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(HabitListActivity::class.java)
+        Intents.init()
+    }
+
+    @After
+    fun destroy() {
+        Intents.release()
+    }
+
+    @Test
+    fun shouldOpenAddHabitActivity() {
+        onView(withId(R.id.fab)).perform(ViewActions.click())
+        intended(hasComponent(AddHabitActivity::class.java.name))
+    }
+}
